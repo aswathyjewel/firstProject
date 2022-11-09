a4e7f1d5c3c7d1ca44f72730a976d1cdbb8ecaa1
aswathyjewel <jewelaswathy@gmail.com>
2022-11-08 21:15:22 -0500

    sout changes

--- src/com/company/day2/Demo.java
+++ src/com/company/day2/Demo.java
@@ -3,5 +3,6 @@ package com.company.day2;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("welcome to the world of java");
+        System.out.println("**");
     }
 }
